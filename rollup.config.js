181a6605b884b5ec0121737323f87170a8e2b1a8
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-29 19:53:19 +0000

    Resolve cjs imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
@@ -9,6 +10,7 @@ export default {
   },
   plugins: [
     resolve(),
+    commonjs(),
     babel({ babelHelpers: 'bundled' })
   ]
 }
