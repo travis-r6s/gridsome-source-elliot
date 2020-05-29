4a3335984432e416bba1c9df3bf8a117ec1cd7b4
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-29 22:07:07 +0000

    Add basic setup to add products & collections

--- src/index.js
+++ src/index.js
@@ -1,27 +1,95 @@
 const got = require('got').default
 
-const { PRODUCTS_QUERY } = require('./queries')
+const { PRODUCTS_QUERY, COLLECTIONS_QUERY } = require('./queries')
+const { ProductSchema, SKUSchema, ImageSchema, SEOSchema, ProductMetadata, CollectionSchema } = require('./schema')
 
-async function ElliotSource (api, options) {
-  const { envs, endpoint = 'https://admin.elliot.store/api' } = options
+const TYPENAMES = {
+  PRODUCT: 'Product',
+  SKU: 'Sku',
+  COLLECTION: 'Collection',
+  IMAGE: 'ElliotImage'
+}
 
+async function ElliotSource (api, options = {}) {
+  // Setup Options & Defaults
+  const { envs, logs = true, endpoint = 'https://admin.elliot.store/api' } = options
+
+  // Checks
   if (!envs) throw new Error('You must provide the Elliot ENV keys.')
 
   const [ELLIOT_DOMAIN_ID, ELLIOT_STORE_FRONT_ID, ELLIOT_STORE_FRONT_NAME, ELLIOT_API_KEY] = envs.split('|')
 
+  // Setup Elliot Client
   const elliot = got.extend({
     headers: { KEY: `KEY ${ELLIOT_API_KEY}` },
     resolveBodyOnly: true,
     responseType: 'json'
-  } )
-
-  api.loadSource( actions => {
-    console.log(actions)
   })
 
-  // const variables = { checkoutId: ELLIOT_STORE_FRONT_ID, domainId: ELLIOT_DOMAIN_ID }
-  // const res = await elliot.post(endpoint, { json: { query: PRODUCTS_QUERY, variables } })
-  // console.log(res)
+  const report = log => logs && console.log(log)
+
+  api.loadSource(async actions => {
+    ELLIOT_STORE_FRONT_NAME && report(`Loading data from ${ELLIOT_STORE_FRONT_NAME} experience...`)
+
+    // Create Collections
+    actions.addCollection(TYPENAMES.COLLECTION)
+    actions.addCollection(TYPENAMES.PRODUCT)
+    actions.addCollection(TYPENAMES.IMAGE)
+    actions.addCollection(TYPENAMES.SKU)
+
+    // Setup Schema
+    actions.addSchemaTypes([ProductSchema, SKUSchema, ImageSchema, SEOSchema, ProductMetadata, CollectionSchema])
+
+    // Load Data
+    await loadCollections(actions)
+    await loadProducts(actions)
+  })
+
+  const loadCollections = async actions => {
+    const collectionStore = actions.getCollection(TYPENAMES.COLLECTION)
+
+    const variables = { checkoutId: ELLIOT_STORE_FRONT_ID, domainId: ELLIOT_DOMAIN_ID }
+    const { data, errors } = await elliot.post(endpoint, { json: { query: COLLECTIONS_QUERY, variables } })
+
+    if (errors) throw new Error(errors[ 0 ].message)
+
+    report(`Added ${data.node.collections.edges.length} collections`)
+    for (const { node: collection } of data.node.collections.edges) {
+      const products = collection.products.edges.map(({ node }) => actions.store.createReference(TYPENAMES.PRODUCT, node.id))
+      const tags = collection.collectionTags.edges.map(({ node }) => node.name)
+
+      collectionStore.addNode({ ...collection, products, tags })
+    }
+  }
+
+  const loadProducts = async actions => {
+    const productStore = actions.getCollection(TYPENAMES.PRODUCT)
+    const imageStore = actions.getCollection(TYPENAMES.IMAGE)
+    const skuStore = actions.getCollection(TYPENAMES.SKU)
+
+    const variables = { checkoutId: ELLIOT_STORE_FRONT_ID, domainId: ELLIOT_DOMAIN_ID }
+    const { data, errors } = await elliot.post(endpoint, { json: { query: PRODUCTS_QUERY, variables } })
+
+    if (errors) throw new Error(errors[ 0 ].message)
+
+    report(`Added ${data.node.products.edges.length} products`)
+    for (const { node: product } of data.node.products.edges) {
+      const skus = product.skus.edges.map(({ node }) => {
+        const skuNode = skuStore.addNode({ ...node, product: actions.store.createReference(TYPENAMES.PRODUCT, product.id) })
+        return actions.store.createReference(skuNode)
+      })
+      const images = product.images.edges.map(({ node }) => {
+        const imageNode = imageStore.addNode(node)
+        return actions.store.createReference(imageNode)
+      })
+      const collections = product.collections.edges.map(({ node }) => actions.store.createReference(TYPENAMES.COLLECTION, node.id))
+
+      const metadata = product.metadata.edges.map(({ node }) => node)
+      const customMetadata = product.customMetadata.edges.map(({ node }) => node)
+
+      productStore.addNode({ ...product, skus, images, collections, metadata, customMetadata })
+    }
+  }
 }
 
 module.exports = ElliotSource
