607c0b4cf6db53d01ff8fce512b5d4c80e682f3e
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-29 19:35:26 +0000

    Add babel plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/gridsome.server.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    resolve(),
+    babel({ babelHelpers: 'bundled' })
+  ]
 }
